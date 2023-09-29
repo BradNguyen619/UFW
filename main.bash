58c007c2c0deadff24ec76e9c6efee136b5d131f
BradNguyen619 <brad.nguyen@mrjaffesclass.com>
2023-09-28 20:12:29 -0700

    Initial commit

---
+++ main.bash
@@ -0,0 +1,6 @@
+# UFW Bash Fire turn on
+# !!!REPLACE THE PASSWORD IN "PASSWORD" WITH THE ACTUAL PASSWORD ->
+# -> ie echo "hansen moy still owes me a kiss"
+
+sudo ufw --force enable
+echo "PASSWORD"
